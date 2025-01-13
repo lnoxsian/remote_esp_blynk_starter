2eec214c512630cf07e492e978751273c0f85cf4
lnoxsian <lnox122224@gmail.com>
2025-01-13 16:35:19 +0530

    Update main_code.ino
    added some features such as the relay being off on startup and other things

--- main_code.ino
+++ main_code.ino
@@ -1,5 +1,5 @@
-#define BLYNK_TEMPLATE_ID "template_id"
-#define BLYNK_TEMPLATE_NAME "template_name"
+#define BLYNK_TEMPLATE_ID "template_id" \\ set the sample temlpate id
+#define BLYNK_TEMPLATE_NAME "template_name" \\ set the sameple template name
 
 #define BLYNK_FIRMWARE_VERSION        "0.1.0"
 
@@ -9,22 +9,22 @@
 
 #include "BlynkEdgent.h"
 
-int wait_time = 5000;
+int wait_time = 1500;
 
 BLYNK_WRITE(V0) // on relay is V0
 {
   int virtual_pin0  = param.asInt();
   if(virtual_pin0 == 1)
   {
-    digitalWrite(12, HIGH);
+    digitalWrite(12, LOW);
     Serial.println("on relay is on for 10s");
     delay(wait_time);
     Serial.println("on relay is off now!!");
-    digitalWrite(12, LOW);
+    digitalWrite(12, HIGH);
   }
   else
   {
-    digitalWrite(12, LOW);
+    digitalWrite(12, HIGH);
   }
 }
 
@@ -33,15 +33,15 @@ BLYNK_WRITE(V1) // off relay is V1
   int virtual_pin1  = param.asInt();
   if(virtual_pin1 == 1)
   {
-    digitalWrite(14, HIGH);
+    digitalWrite(14, LOW);
     Serial.println("off relay is on for 10s");
     delay(wait_time);
     Serial.println("off relay is off now!!");
-    digitalWrite(14, LOW);
+    digitalWrite(14, HIGH);
   }
   else
   {
-    digitalWrite(14, LOW);
+    digitalWrite(14, HIGH);
   }
 }
 
@@ -49,6 +49,12 @@ void setup()
 { 
   pinMode(14, OUTPUT);
   pinMode(12, OUTPUT);
+  digitalWrite(12, HIGH);
+  digitalWrite(14, HIGH);
+  delay(2000);
+  digitalWrite(14, LOW);
+  delay(1500);
+  digitalWrite(14, HIGH);
   Serial.begin(115200);
   Serial.println("Serial has .. started");
   delay(100);
@@ -58,6 +64,5 @@ void setup()
 
 void loop() 
 {
-  Serial.println("BlynkEdgent has .. started to run");
   BlynkEdgent.run();
 }
